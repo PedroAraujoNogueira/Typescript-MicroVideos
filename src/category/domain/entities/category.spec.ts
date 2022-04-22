9e9f29094431ae3a7127b3564047b31be9b746d4
PedroAraujo <pedro.nogueira@contazap.com>
2022-04-22 12:33:26 -0300

    Teste constructor da entidade categoria

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,7 +2,27 @@ import { Category } from './category';
 
 describe("Category Unit Tests", () => {
     test('Constructor of category', () => {
-        const category = new Category('Movie');
-        expect(category.name).toBe('Movie')
+
+        const created_at = new Date();
+
+        // Arrange and Act together.
+        const category = new Category({
+            name: "Movie",
+            description: "description",
+            is_active: true,
+            created_at,
+        });
+
+        // Assert
+        expect(category.props).toStrictEqual({
+            name: "Movie",
+            description: "description",
+            is_active: true,
+            created_at,  
+        })
+        // expect(category.name).toBe("Movie")
+        // expect(category.description).toBe("description")
+        // expect(category.is_active).toBeTruthy()
+        // expect(category.created_at).toBe(created_at)
     })
 })
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -2,8 +2,7 @@ export type CategoryProperties = {
     name: string;
     description?: string;
     is_active?: boolean;
-    created_at?: Date;
-}
+    created_at?: Date;}
 
 export class Category {
     constructor(public props: CategoryProperties){}
